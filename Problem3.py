809ad02181b7697fa32e06c450c381b5b150927c
Aranaris <vinceye.cm@gmail.com>
2014-10-21 22:43:58 -0400

    Problem 3 update1

--- Problem3.py
+++ Problem3.py
@@ -2,7 +2,7 @@
 
 #Problem 3: Largest prime factor
 
-x = 13195
+x = 600851475143
 i = 1
 
 is_prime = 0
@@ -13,16 +13,18 @@ while i <= x // 2:
 	if x % i == 0 and i % 2 != 0:
 		factor = x // i
 		listf.append(factor)
+		j = 2
+		while j <= factor // 2:
+			if factor % j == 0:
+				break
+			j += 1
+		if j >= factor // 2:
+			max_prime = factor
+			break
 	i += 1
 
 # print(listf)
-for a in listf:
-	j = 2
-	while j <= a // 2:
-		if a % j == 0:
-			break
-		j += 1
-	if j >= a // 2 and a > max_prime:
-		max_prime = a
+
+	
 		# print(j)
 print(max_prime)
