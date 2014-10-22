7a1b59ba2a85f56ca93e428f213d876fd5a46af1
Aranaris <vinceye.cm@gmail.com>
2014-10-21 20:46:29 -0400

    Problem 1 remake
    change while to for

--- Problem1.py
+++ Problem1.py
@@ -1,16 +1,11 @@
 #Project Euler Problems
 
 #Problem 1: Multiples of 3 and 5
-i = 1
-list1 = []
-while i < 1000:
-	if i % 3 == 0:
-		if i % 15 != 0:
-			list1.append(i)
-	if i % 5 == 0:
-		list1.append(i)
 
-	i += 1
+list1 = []
+for i in range (1, 1000):
+	if i % 3 == 0 or i % 5 == 0:
+		list1.append(i)
 
 #print (list1)
 print (sum(list1))
