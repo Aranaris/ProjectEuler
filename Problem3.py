bdd099465d41171231334f4153c78f28b0495f3c
Aranaris <vinceye.cm@gmail.com>
2014-10-28 20:41:28 -0400

    Redoing Problem 3

--- Problem3.py
+++ Problem3.py
@@ -2,29 +2,12 @@
 
 #Problem 3: Largest prime factor
 
-x = 600851475143
-i = 1
+#y = 600851475143
+y = 24
 
-is_prime = 0
-# listf = []
-max_prime = 0
-
-while i <= x // 2:
-	if x % i == 0 and i % 2 != 0:
-		factor = x // i
-		# listf.append(factor)
-		j = 2
-		while j <= factor // 2:
-			if factor % j == 0:
-				break
-			j += 1
-		if j >= factor // 2:
-			max_prime = factor
-			break
-	i += 1
-
-# print(listf)
-
-	
-		# print(j)
-print(max_prime)
+def prime_factors(n):
+	i = 1
+	while i <= n:
+		if n % i == 0 and i > 1:
+			return i
+		i += 1
