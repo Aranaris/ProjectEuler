b401fa8d7580c84e5b8cfa50fd5164451ee9934f
Aranaris <vinceye.cm@gmail.com>
2014-10-28 21:16:48 -0400

    Problem3v3

--- Problem3.py
+++ Problem3.py
@@ -4,14 +4,15 @@
 
 y = 600851475143
 #y = 24
-factors = []
+
 def prime_factors(n):
 	i = 2
 	while i <= n:
 		if n % i == 0: 
+			factors = [i]
 			factors.append(prime_factors(n // i))
-			return i
+			return factors
 		i += 1
 
-prime_factors(y)
-print(factors)
+a = prime_factors(y)
+print(a)
