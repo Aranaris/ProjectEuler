8d08ad851de7cec94136a3a593ea3af067179435
Aranaris <vinceye.cm@gmail.com>
2014-10-28 21:01:45 -0400

    Problem3Test

--- Problem3.py
+++ Problem3.py
@@ -2,12 +2,16 @@
 
 #Problem 3: Largest prime factor
 
-#y = 600851475143
-y = 24
-
+y = 600851475143
+#y = 24
+factors = []
 def prime_factors(n):
 	i = 2
 	while i <= n:
-		if n % i == 0:
+		if n % i == 0: 
+			factors.append(prime_factors(n // i))
 			return i
 		i += 1
+
+prime_factors(y)
+print(factors)
