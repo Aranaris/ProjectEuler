71739b1571a9f805b6dbb1eab19a870951f42f16
Aranaris <vinceye.cm@gmail.com>
2014-11-10 20:40:45 -0500

    Problem 4 Updated

--- Problem4.py
+++ Problem4.py
@@ -2,26 +2,29 @@
 
 #Problem 4: Largest palindrome product
 
-p1 = 100
-p2 = 100
-max_palindrome = 0
+
+
 
 def check_palindrome(n):
-	test = list(map(int, str(n)))
-	i = 0
-	while i < len(test) // 2:
-		if test[i] != test[len(test) - 1 - i]:
-			return False
-		i += 1
-	return True
-	
-while p1 < 1000:
-	while p2 < 1000:
-		if check_palindrome(p1 * p2) and p1 * p2 > max_palindrome:
-			max_palindrome = p1 * p2
-		p2 += 1
-	p1 += 1
-	p2 = 100
+	test = str(n)
+	if test[0 : len(test) // 2] == test[: len(test) // 2 - 1 : -1]:
+		return True
+	return False
 
+def max_palindrome(a, b):
+	p1 = 100
+	p2 = 100
+	max_pal = 0
+	while p1 <= a:
+		while p2 <= b:
+			if (p1 * p2) > max_pal and check_palindrome(p1 * p2): #and :
+				max_pal = p1 * p2
+			p2 += 1	
+		p1 += 1
+		p2 = 100
+	return max_pal
 # print(str(max1) + " " + str(max2))
-print (max_palindrome)
+print (max_palindrome(999, 999))
+
+
+		
