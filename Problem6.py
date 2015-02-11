212bdeaa2d714a3d501938b74510883ae85328cd
Aranaris <vinceye.cm@gmail.com>
2015-02-11 13:47:03 -0500

    Fixed Problem6 no while loop

--- Problem6.py
+++ Problem6.py
@@ -2,27 +2,19 @@
 
 #Problem 6: Sum Square Difference
 
-import array
 import sys
 
 
 
 def sum_of_squares(n): # sum of 1^2 + 2^2 + ... (n - 1)^2 + n^2
-	i = 1
 	sum1 = 0
-	while i <= n:
-		sum1 += i ** 2
-		i += 1
-	
+	for x in range(n+1):
+		sum1 += x * x	
 	return sum1
 	
 def square_of_sum(n): # square of (1 + 2 + ... + (n - 1) + n)
-	i = 1
-	sum1 = 0
-	while i <= n:
-		sum1 += i
-		i += 1
-	return sum1 ** 2
+	return sum(range(n+1)) ** 2
+	
 
 
 x = int(sys.argv[1])
