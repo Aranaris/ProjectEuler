040972cce687e6867271808a803f555e9d0371e1
Aranaris <vinceye.cm@gmail.com>
2015-03-05 15:15:18 -0500

    Updated to Problem 16

--- Problem16.py
+++ Problem16.py
@@ -1,19 +1,11 @@
-
-# coding: utf-8
-
-# In[7]:
-
 ##Project Euler Problems
 
 #Problem 16: Power digit sum
 
 def powersum(x):
-    return sum([int(i) for i in str(x)])
+    return sum(int(i) for i in str(x))
 
 print(powersum(2 ** 1000))
-
-
-# In[ ]:
 
 
 
